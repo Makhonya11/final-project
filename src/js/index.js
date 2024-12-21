fdb35041f1e61d634fa6d69be01a811521c343d8
Makhach <mmaaamm666@gmail.com>
2024-12-21 20:38:02 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import "swiper/modules/pagination.css";
+import 'swiper/swiper-bundle.min.css';
+import '../scss/style.scss';
+
+
+import brandSwiper from '../js/brands-swiper';
+import menu from '../js/script-menu';
+import modal from '../js/modal-windows';
+
